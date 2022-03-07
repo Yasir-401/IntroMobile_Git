cc2de17e9428233bf1301a4e0c87f2c2d695dfa7
Yasir-401 <yassser.mohammmed122@gmail.com>
2022-03-07 13:39:44 +0100

    add comments

--- products-backend/routes/index.js
+++ products-backend/routes/index.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.redirect('/products/');
+router.get("/", function (req, res) {
+  res.redirect("/products/");
 });
 
 module.exports = router;
 
-
-
+//adding a comment here as well
